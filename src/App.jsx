43f435a0470bd33c3d08e99f9043038c8cbce8b1
mrmuhib300@gmail.com <mrmuhib300@gmail.com>
2024-11-24 22:22:24 +0600

    Product Deshbord

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container mx-auto p-2">
+      <h1 className="font-semibold text-xl text-current">Products Dashboard</h1>
+    </div>
+  );
+};
+
+export default App;
